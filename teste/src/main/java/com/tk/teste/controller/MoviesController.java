803af7d1c16d7026ef077aaef3f0175b3640aa29
sopmacamilla <camillaoui@gmail.com>
2021-07-18 13:23:32 -0300

    feat: inserido método post

--- teste/src/main/java/com/tk/teste/controller/MoviesController.java
+++ teste/src/main/java/com/tk/teste/controller/MoviesController.java
@@ -3,10 +3,13 @@ package com.tk.teste.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,4 +44,10 @@ public class MoviesController {
 	public ResponseEntity<List<Movies>> getByGenres(@PathVariable String genres) {
 		return ResponseEntity.ok(repository.findAllByGenresContainingIgnoreCase(genres));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Movies> post(@RequestBody Movies movies){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(movies));
+	}
+		 
 }
