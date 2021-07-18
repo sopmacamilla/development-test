1a86bbae119c8d40e040c5be806e56cba625001c
sopmacamilla <camillaoui@gmail.com>
2021-07-18 13:36:15 -0300

    feat: inserido método put

--- teste/src/main/java/com/tk/teste/controller/MoviesController.java
+++ teste/src/main/java/com/tk/teste/controller/MoviesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,11 @@ public class MoviesController {
 	@PostMapping
 	public ResponseEntity<Movies> post(@RequestBody Movies movies){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(movies));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Movies> put (@RequestBody Movies movies){  
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(movies));
 	}
 		 
 }
