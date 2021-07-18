2dcf0be1763d6769b3efbfa1329ff8b329f6fdaf
sopmacamilla <camillaoui@gmail.com>
2021-07-18 13:42:28 -0300

    feat: inserido método delete

--- teste/src/main/java/com/tk/teste/controller/MoviesController.java
+++ teste/src/main/java/com/tk/teste/controller/MoviesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,11 @@ public class MoviesController {
 	@PutMapping
 	public ResponseEntity<Movies> put (@RequestBody Movies movies){  
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(movies));
+	}
+	
+	@DeleteMapping("/{movieId}")
+	public void delete(@PathVariable long movieId) {
+		repository.deleteById(movieId);
 	}
 		 
 }
