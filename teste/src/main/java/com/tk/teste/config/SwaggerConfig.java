90bd487694f18e82d80377f850a3f460c2a33f97
sopmacamilla <camillaoui@gmail.com>
2021-07-18 16:21:45 -0300

    feat: Configurações de Swagger

---
+++ teste/src/main/java/com/tk/teste/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.tk.teste.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.tk.teste.controller"))
+                .paths(PathSelectors.any()).build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Movie API REST").description("Aplicação para filmes").version("1.0")
+                .contact(contact()).build();
+    }
+
+    private Contact contact() {
+        return new Contact("Camilla Campos", "https://github.com/sopmacamilla",
+                "Dev Full Stack");
+    }
+}
