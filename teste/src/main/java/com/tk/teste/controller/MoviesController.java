62adc2f385d28491f30db9ed217c0699e63a93b5
sopmacamilla <camillaoui@gmail.com>
2021-07-18 16:32:22 -0300

    inseridas especificações de execução dos endpoints no controller

--- teste/src/main/java/com/tk/teste/controller/MoviesController.java
+++ teste/src/main/java/com/tk/teste/controller/MoviesController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tk.teste.model.Movies;
 import com.tk.teste.repository.MoviesRepository;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/movies")
 @CrossOrigin("*")
@@ -26,37 +28,44 @@ public class MoviesController {
 	@Autowired
 	private MoviesRepository repository;
 	
+	@ApiOperation(value = "consulta de todos os filmes")
 	@GetMapping
 	public ResponseEntity<List<Movies>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
 	
+	@ApiOperation(value = "consulta um filme através do id")
 	@GetMapping("/{movieId}")
 	public ResponseEntity<Movies> getByMovieId(@PathVariable long movieId) {
 		return repository.findById(movieId).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
+	@ApiOperation(value = "consulta um filme através de uma parte de um título")
 	@GetMapping("title/{title}")
 	public ResponseEntity<List<Movies>> getByTitle(@PathVariable String title) {
 		return ResponseEntity.ok(repository.findAllByTitleContainingIgnoreCase(title));
 	}
 	
+	@ApiOperation(value = "consulta um filme através de um gênero")
 	@GetMapping("genres/{genres}")
 	public ResponseEntity<List<Movies>> getByGenres(@PathVariable String genres) {
 		return ResponseEntity.ok(repository.findAllByGenresContainingIgnoreCase(genres));
 	}
 	
+	@ApiOperation(value = "insere um filme")
 	@PostMapping
 	public ResponseEntity<Movies> post(@RequestBody Movies movies){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(movies));
 	}
 	
+	@ApiOperation(value = "alteração de um filme")
 	@PutMapping
 	public ResponseEntity<Movies> put (@RequestBody Movies movies){  
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(movies));
 	}
 	
+	@ApiOperation(value = "exclui um filme através do id")
 	@DeleteMapping("/{movieId}")
 	public void delete(@PathVariable long movieId) {
 		repository.deleteById(movieId);
